f08d50dfcd04ffd012ed936b1e56054e48e53e35
MeinLiX <meinlix@ukr.net>
2022-02-14 00:30:23 +0200

    [L1] add bot for demo dialogue logic

--- Phatic_dialogue_L1/Program.cs
+++ Phatic_dialogue_L1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Phatic_dialogue_L1.Source;
+using Telegram.Bot;
+
+dotenv.net.DotEnv.Load();
+string TELEGRAM_BOT_TOKEN = Environment.GetEnvironmentVariable("TELEGRAM_BOT_TOKEN") ?? throw new NullReferenceException("TELEGRAM_BOT_TOKEN");
+
+new TelegramBotClient(TELEGRAM_BOT_TOKEN).StartReceiving(new Telegram.Bot.Extensions.Polling.DefaultUpdateHandler(HandleUpdateAsync, HandleErrorAsync));
+async Task HandleErrorAsync(ITelegramBotClient bc, Exception err, CancellationToken ct) => Console.WriteLine(err.Message);
+async Task HandleUpdateAsync(ITelegramBotClient botClient, Telegram.Bot.Types.Update update, CancellationToken ct)
+{
+    if (update.Type != Telegram.Bot.Types.Enums.UpdateType.Message && update?.Message?.Text is null) return;
+    await botClient.SendTextMessageAsync(update.Message.Chat.Id, text: Brain.GetAnswer(update.Message.Text), cancellationToken: ct);
+}
+Console.ReadKey();
---
+++ Phatic_dialogue_L1/Source/Brain.cs
@@ -0,0 +1,7 @@
+﻿namespace Phatic_dialogue_L1.Source
+{
+    public static class Brain
+    {
+        public static string GetAnswer(string input) => input.Trim().ToLower();
+    }
+}
